e3c5c4f653d23611ece89bfcc5b3c433e3d9b89b
vanheaven-ui <vanheaven6@gmail.com>
2020-09-01 19:38:22 +0300

    Add delete comment code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
     @comment.destroy
-
-    redirect_to article_path(@comment.article)
+    
+    redirect_to article_path(@article)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :articles do
-    resources :comments 
+    resources :comments
   end
 end
