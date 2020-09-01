1154873044aa7d7ec43b25e00b456f6fa95c3bb8
vanheaven-ui <vanheaven6@gmail.com>
2020-09-02 01:01:31 +0300

    Complete Iteration 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'articles#index'
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_201026) do
+ActiveRecord::Schema.define(version: 2020_09_01_202153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_201026) do
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
